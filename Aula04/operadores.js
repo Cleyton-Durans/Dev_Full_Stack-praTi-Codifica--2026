a86925d1527b5cd6364f98501ddbfbd1a983c1de
Cleyton Durans <Cleytondurans.n@gmail.com>
2026-05-07 14:44:19 -0300

    atualizado os operadores com mais infomações

--- Aula04/operadores.js
+++ Aula04/operadores.js
@@ -3,15 +3,14 @@ let numero = 0
 console.log(numero > 0 ? "Positivo" : numero < 0 ? "Negativo" : "Zero")
 let media = 6
 console.log(media >= 6 ? "Aprovado" : "Reprovado")
+console.log("_______________________________________")
 
-//Operadores Relacionais
-/*
-    >
-    <
-    >=
-    <=
-    ==
-    ===
-    !=
-    !==
-*/  
+console.log("Operadores Relacionais 5 e 10")
+ console.log("Maior que >: " + (5 > 10))
+    console.log("Menor que <: " + (5 < 10)) 
+    console.log("Maior ou igual que >=: " + (5 >= 10))  
+    console.log("Menor ou igual que <=: " + (5 <= 10))  
+    console.log("Igualdade ==: " + (5 == 10))
+    console.log("Igualdade estrita ===: " + (5 === 10))
+    console.log("Diferente !=: " + (5 != 10))
+    console.log("Diferente estrito !==: " + (5 !== 10))
