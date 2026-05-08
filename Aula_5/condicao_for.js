91f53e0bbc272e16a346ffb516a828424f10be90
Cleyton Durans <Cleytondurans.n@gmail.com>
2026-05-08 16:53:58 -0300

    incluindo aula

---
+++ Aula_5/condicao_for.js
@@ -0,0 +1,16 @@
+// for(inicialização; condição; incremento ou decremento)
+
+// Cria uma repetição de X vezes.
+// Usamos quando sabemos quantas vezes o código vai repetir.
+console.log("Contagem decrescente com for:")
+for(let i = 5; i >= 0; i--){
+    console.log(i)
+}
+
+console.log("\n")
+console.log("Contagem crescente com while:")
+let j = 0
+while(j <= 5){
+    console.log(j)
+    j++
+}
