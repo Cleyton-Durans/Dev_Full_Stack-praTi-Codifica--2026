aabf9cb4fcc1d9a435549124cb62227b732dcbe3
Cleyton Durans <Cleytondurans.n@gmail.com>
2026-05-08 15:55:31 -0300

    Incluindo aulas

---
+++ Aula_5/condicaoMultiplas.js
@@ -0,0 +1,197 @@
+let dia = new Date()
+let diaSemana = dia.getDay()
+let diaMes = dia.getDate()
+/*
+    0 - Domingo
+    1 - Segunda
+    2 - Terça
+    3 - Quarta
+    4 - Quinta
+    5 - Sexta
+    6 - Sabado
+*/
+
+switch(diaSemana){
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda-feira")
+        break
+    case 2:
+        console.log("Terça-feira")
+        break
+    case 3:
+        console.log("Quarta-feira")
+        break
+    case 4:
+        console.log("Quinta-feira")
+        break
+    case 5:
+        console.log("Sexta-feira")
+        break
+    case 6:
+        console.log("Sabado")
+        break
+    default:
+        console.log("Valor invalido")
+        break
+}
+
+/* Switch Case meses do ano */
+let mes = dia.getMonth()
+/*
+    0 - Janeiro
+    1 - Fevereiro
+    2 - Março
+    3 - Abril
+    4 - Maio
+    5 - Junho
+    6 - Julho
+    7 - Agosto
+    8 - Setembro
+    9 - Outubro
+    10 - Novembro
+    11 - Dezembro
+*/
+
+switch(mes){
+    case 0:
+        console.log("janeiro")
+        break
+    case 1:
+        console.log("fevereiro")
+        break
+    case 2:
+        console.log("março")
+        break
+    case 3:
+        console.log("abril")
+        break
+    case 4:
+        console.log("maio")
+        break
+    case 5:
+        console.log("junho")
+        break
+    case 6:
+        console.log("julho")
+        break
+    case 7:
+        console.log("agosto")
+        break
+    case 8:
+        console.log("setembro")
+        break
+    case 9:
+        console.log("outubro")
+        break
+    case 10:
+        console.log("novembro")
+        break
+    case 11:
+        console.log("dezembro")
+        break
+    default:
+        console.log("Valor invalido")
+        break
+}
+
+switch(diaMes){
+    case 0:
+        console.log("1º dia do mês")
+        break
+    case 1:
+        console.log("2º dia do mês")    
+        break
+    case 2:
+        console.log("3º dia do mês")
+        break
+    case 3:
+        console.log("4º dia do mês")
+        break
+    case 4:
+        console.log("5º dia do mês")
+        break
+    case 5:
+        console.log("6º dia do mês")
+        break
+    case 6:
+        console.log("7º dia do mês")
+        break
+    case 7:
+        console.log("8º dia do mês")
+        break
+    case 8:
+        console.log("9º dia do mês")
+        break
+    case 9:
+        console.log("10º dia do mês")
+        break
+    case 10:
+        console.log("11º dia do mês")
+        break
+    case 11:
+        console.log("12º dia do mês")
+        break
+    case 12:
+        console.log("13º dia do mês")
+        break
+    case 13:
+        console.log("14º dia do mês")
+        break
+    case 14:
+        console.log("15º dia do mês")
+        break
+    case 15:
+        console.log("16º dia do mês")
+        break
+    case 16:
+        console.log("17º dia do mês")
+        break   
+    case 17:
+        console.log("18º dia do mês")
+        break
+    case 18:
+        console.log("19º dia do mês")
+        break
+    case 19:
+        console.log("20º dia do mês")
+        break
+    case 20:
+        console.log("21º dia do mês")
+        break
+    case 21:
+        console.log("22º dia do mês")
+        break
+    case 22:
+        console.log("23º dia do mês")
+        break
+    case 23:
+        console.log("24º dia do mês")
+        break
+    case 24:
+        console.log("25º dia do mês")
+        break
+    case 25:
+        console.log("26º dia do mês")
+        break
+    case 26:
+        console.log("27º dia do mês")
+        break
+    case 27:
+        console.log("28º dia do mês")
+        break
+    case 28:
+        console.log("29º dia do mês")
+        break
+    case 29:
+        console.log("30º dia do mês")
+        break
+    case 30:
+        console.log("31º dia do mês")
+        break
+    default:
+        console.log("Valor invalido")
+        break
+}
