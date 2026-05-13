483f1116f91b03cd4393cc407ee98c79db359f79
Yasmin <cleytondurans.n@gmail.com>
2026-05-13 00:56:07 -0300

    Incluindo aula 7

---
+++ Aula_7/prompt.js
@@ -0,0 +1,6 @@
+let PROMPT = require('prompt-sync')();
+let nome = PROMPT("Digite seu nome: ");
+console.log(`Olá, ${nome}! Bem-vindo ao curso de Java Script.`);
+
+/* Instalação do prompt-sync: npm install prompt-sync */
+/* npm é um gerenciador de pacotes */
