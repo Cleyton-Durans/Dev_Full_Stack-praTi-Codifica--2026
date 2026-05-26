48ca55a8e894533d3e4f181ed85e3c8a562b57ba
Cleyton-Durans <Cleytondurans.n@gmail.com>
2026-05-26 01:35:43 -0300

    Atualizando exercicios

---
+++ Exercicio_avaliativo1/atividade11.js
@@ -0,0 +1,35 @@
+/* 11. Crie um programa que leia o nome e a idade de 9 pessoas e guarde esses valores em
+dois vetores, em posições relacionadas. No final, mostre uma listagem contendo apenas
+os dados das pessoas menores de idade. */
+const prompt = require('prompt-sync')();
+
+function lerDados() {
+   
+    let nomes = [];
+    let idades = [];
+    let maiorDeIdade = []
+
+    for (let i = 0; i < 2; i++) {
+        let nomePessoa = prompt(`Digite o nome da pessoa ${i + 1}: `)
+        let idadePessoa = parseInt(prompt(`Digite a idade da pessoa ${i + 1}: `))
+
+        nomes.push(nomePessoa)
+        idades.push(idadePessoa)
+
+        if (idades > 18 ) {
+            maiorDeIdade.push(nomePessoa)
+        }
+    }
+
+    return {
+        nomes: nomes,
+        idades: idades,
+        maiorDeIdade: maiorDeIdade
+    }
+}
+
+let resultado = lerDados();
+console.log("\n -- Apresenta Resultado -- ")
+console.log(`Nomes: ${resultado.nomes}`)
+console.log(`Idades: ${resultado.idades}`)
+console.log(`Maiores de idades: ${resultado.maiorDeIdade}`)
---
+++ Exercicio_avaliativo1/atividade17.js
@@ -0,0 +1,3 @@
+/* 17. Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de
+idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui
+nome e idade. */
